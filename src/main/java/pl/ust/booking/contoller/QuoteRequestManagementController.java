41a190703928921e41e09513733381d167a08f1e
darco2018 <newsecond@outlook.com>
2018-12-04 19:14:54 +0100

    Add more methods to QuoteRequestManagmentController

--- src/main/java/pl/ust/booking/contoller/QuoteRequestManagementController.java
+++ src/main/java/pl/ust/booking/contoller/QuoteRequestManagementController.java
@@ -5,6 +5,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
 import pl.ust.booking.model.QuoteRequest;
 
@@ -24,9 +26,40 @@ public class QuoteRequestManagementController {
 	}
 
 	@GetMapping("/quoteRequests/{quoteId}")
-	public String viewQuoteRequest(@PathVariable int quoteId) {
-
-		return "quoteRequestDetails";
+	public ModelAndView viewQuoteRequest(@PathVariable int quoteId) {
+		
+		QuoteRequest quoteRequest = new QuoteRequest(); // it would normally be returned by service
+		quoteRequest.setBudget(5000);
+		quoteRequest.setEventType("wedding");
+		
+		ModelAndView mav = new ModelAndView();
+		mav.addObject("quoteRequest", quoteRequest);
+		mav.setViewName("quoteRequestDetails");
+		
+		return mav;
+	}
+	
+	@GetMapping("/quoteRequest/social/{id}")
+	public String viewRequestSocial(@PathVariable int id) {
+		
+		String returnViewName = "quoteRequestSocialEventDetail";
+		
+		boolean someCondition = true;
+		if(someCondition) {
+			returnViewName = "redirect:/quoteRequest/wedding/{id}"; // go to different controller
+		}
+		
+		return returnViewName;
+	}
+	
+	@GetMapping
+	@ResponseBody
+	public QuoteRequest viewQuoteRequestApi() {  //should be somewhere else in fact
+		
+		QuoteRequest quoteRequest = new QuoteRequest(); // it would normally be returned by service
+		quoteRequest.setBudget(5000);
+		
+		return quoteRequest; // conversion to JSON
 	}
 
 	@PostMapping("/quoteDetail")
