7004283a75c66f26cb5d5700532fc7c4f717d4bd
darco2018 <newsecond@outlook.com>
2018-12-04 19:14:54 +0100

    Add more methods to QuoteRequestManagmentController

--- src/main/java/pl/ust/booking/model/QuoteRequest.java
+++ src/main/java/pl/ust/booking/model/QuoteRequest.java
@@ -2,4 +2,14 @@ package pl.ust.booking.model;
 
 public class QuoteRequest {
 
+	public void setBudget(int budget) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void setEventType(String eventType) {
+		// TODO Auto-generated method stub
+		
+	}
+
 }
