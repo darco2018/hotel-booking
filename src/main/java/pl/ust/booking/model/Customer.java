7c7da940319e2a9c3106510155fc34304505c2f2
darco2018 <newsecond@outlook.com>
2018-12-05 15:38:51 +0100

    Bulk varied additions

---
+++ src/main/java/pl/ust/booking/model/Customer.java
@@ -0,0 +1,12 @@
+package pl.ust.booking.model;
+
+public class Customer {
+	
+	private String lastName;
+	private String firstName;
+	private String emailAddress;
+	private String phoneNumber;
+	
+	
+
+}
---
+++ src/main/java/pl/ust/booking/model/SalesRep.java
@@ -0,0 +1,8 @@
+package pl.ust.booking.model;
+
+public class SalesRep {
+	private String lastName;
+	private String firstName;
+	private String emailAddress;
+	private String phoneNumber;
+}
